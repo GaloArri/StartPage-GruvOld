0cc90adbd80e893c45b12dd01fd3238399eb745b
GaloArri <galo.arri172186@alumnos.info.unlp.edu.ar>
2023-12-23 14:10:02 -0300

    StartPage

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function date() {
+  let currentDate = new Date();
+  let dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+  let date = currentDate.toLocaleDateString("en-GB", dateOptions);
+  document.getElementById("header_date").innerHTML = date;
+}
+
+function loadFunctions() {
+  date();  
+
+}
+
+
